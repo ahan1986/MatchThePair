227c8eb7c71895a06172619092e9af39c154415d
Handrew <andrew.a.han@gmail.com>
2018-06-26 18:27:32 -0400

    added an alt with the pokeball image

--- src/components/TopMidBody/TopMidBody.js
+++ src/components/TopMidBody/TopMidBody.js
@@ -5,7 +5,7 @@ const TopMidBody = props => (
     <div className="topmidbodyDiv">
         <h1>Clicky Game!</h1>
         <h2>Click on an image to earn points, but don't click on any more than once!</h2>
-        <img src="http://pngimg.com/uploads/pokeball/pokeball_PNG5.png" />
+        <img alt="pokeball moving side to side" src="http://pngimg.com/uploads/pokeball/pokeball_PNG5.png" />
     </div>
 );
 
